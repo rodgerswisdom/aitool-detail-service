a1a85a960187a45ccd7e079ef482c2e9a366412b
HAWONA RODGERS WISDOM <130440506+rodgerswisdom@users.noreply.github.com>
2024-12-24 21:13:21 +0300

    Update settings.py

--- tool_service/settings.py
+++ tool_service/settings.py
@@ -171,12 +171,15 @@ STATIC_ROOT = BASE_DIR/'staticfiles'
 STATIC_URL = '/static/'
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
+"""
+REFACTORING ADMIN STYLES
+"""
+# if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 REST_FRAMEWORK = {
