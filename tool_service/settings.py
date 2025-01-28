6aa73de23ab717f8eed4aca53c6c39b38a987070
rodgerswisdom <trulyhawona@gmail.com>
2025-01-28 17:34:25 +0300

    Automated commit

--- tool_service/settings.py
+++ tool_service/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_!c4&%oi8d$jl+#sbxgc=$#2l^#gl64v#ti%ianxk*n&lv!0vu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 # https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
@@ -114,6 +114,20 @@ DATABASES = {
     )
 }
 
+
+# settings.py
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aitool_detail_service',
+        'USER': 'ratego',
+        'PASSWORD': 'ubuntu/2025',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
